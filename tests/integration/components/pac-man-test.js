6ea48d660c7c91dbf095030b8ed6c663d3af51ce
JasonYu <yuhang@tapatalk.com>
2017-01-10 12:32:57 +0800

    Remove the component test.
    Signed-off-by: JasonYu <yuhang@tapatalk.com>

--- tests/integration/components/pac-man-test.js
+++ tests/integration/components/pac-man-test.js
@@ -1,25 +1,13 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent } from 'ember-qunit';
+// import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('pac-man', 'Integration | Component | pac man', {
-  integration: false
+  integration: true
 });
 
-test('it renders', function(assert) {
+// test('it renders', function(assert) {
 
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{pac-man}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#pac-man}}
-      template block text
-    {{/pac-man}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
-});
+// });
